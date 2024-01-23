b9455cc639e479b72e277e9c0d1726b7b73b737f
miorezu <m.miorezu@gmail.com>
2024-01-23 17:44:18 +0200

    Fire eye changes

--- scripts/fire_eye.gd
+++ scripts/fire_eye.gd
@@ -3,8 +3,8 @@ extends CharacterBody2D
 var timer := Timer.new()
 var damage_timer := Timer.new()
 @onready var damage_area = $damage_area
-@onready var fire_particles = $fire_particles
 @onready var eye_animation = $sprite
+@onready var shadow_animation = $shadow_under
 var spell_duration = 9
 var tick_time = 1
 var enemy_inside = []
@@ -16,12 +16,12 @@ func _ready():
 	add_child(timer)
 	damage_timer.one_shot = false
 	add_child(damage_timer)
-	global_position = get_global_mouse_position()
+	#global_position = get_global_mouse_position()
 	eye_animation.play("opening")
 	await eye_animation.animation_finished
 	print("animation_finished")
 	set_process(true)
-	$area_paricles.emitting = true
+	$area_particles.emitting = true
 	eye_animation.play("searching")
 	damage_area.connect("body_entered", Callable(self, "_on_damage_area_body_entered"))
 	damage_area.connect("body_exited", Callable(self, "_on_damage_area_body_exited"))
@@ -35,9 +35,7 @@ func _ready():
 	queue_free()
 
 func _process(delta):
-	z_index = global_position.y / 2
 	eye_animation.position.y = sin(Time.get_ticks_msec()  * 0.003) * 3
-	
 
 func deal_damage(body):
 	while enemy_inside.has(body):
