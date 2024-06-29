e3bf002a687f2892729f09285fb2d437a31690b7
fade <dimabreusgo@gmail.com>
2024-06-29 18:55:04 +0300

    removed unused files, new game ui bars

--- scripts/ui/PlayerBars.cs
+++ scripts/ui/PlayerBars.cs
@@ -4,40 +4,23 @@ namespace projectpinky.scripts.ui;
 
 public partial class PlayerBars : Node
 {
-    private TextureRect hpBar;
-    private ShaderMaterial hpBarMaterial;
-    private Label currentHp;
-    private Label maxHpLabel;
-
-    private TextureRect manaBar;
-    private ShaderMaterial manaBarMaterial;
-    private Label currentMana;
-    private Label maxManaLabel;
+    [Export] private TextureProgressBar hpBar;
+    [Export] private TextureProgressBar manaBar;
 
     public override void _Ready()
     {
-        hpBar = GetNode<TextureRect>("hp_bar");
-        hpBarMaterial = hpBar.Material as ShaderMaterial;
-        currentHp = GetNode<Label>("current_hp");
-        maxHpLabel = GetNode<Label>("max_hp");
 
-        manaBar = GetNode<TextureRect>("mana_bar");
-        manaBarMaterial = manaBar.Material as ShaderMaterial;
-        currentMana = GetNode<Label>("current_mana");
-        maxManaLabel = GetNode<Label>("max_mana");
     }
 
     public void UpdateHpValue(int hp, int maxHp)
     {
-        hpBarMaterial.SetShaderParameter("fV", (float)hp / maxHp);
-        currentHp.Text = hp.ToString();
-        maxHpLabel.Text = maxHp.ToString();
+        hpBar.MaxValue = maxHp;
+        hpBar.Value = hp;
     }
 
     public void UpdateManaValue(int mana, int maxMana)
     {
-        manaBarMaterial.SetShaderParameter("fV", (float)mana / maxMana);
-        currentMana.Text = mana.ToString();
-        maxManaLabel.Text = maxMana.ToString();
+        manaBar.MaxValue = maxMana;
+        manaBar.Value = mana;
     }
 }
