c18c5ec71265536d30f9e4cb9ae61ebc26e00a47
fade <dimabreusgo@gmail.com>
2024-06-30 22:32:13 +0300

    hitbox statuses

--- scripts/DamageHpSystem/Hitbox.cs
+++ scripts/DamageHpSystem/Hitbox.cs
@@ -6,6 +6,8 @@ public partial class Hitbox : Area2D
     [Signal]
     public delegate void EntityEnteredEventHandler();
 
+    [Export] private Hurtbox.Statuses status;
+    [Export] private double statusDuration;
     [Export] private int Damage;
 
     private void OnHurtBoxEntered(Node2D body)
@@ -13,6 +15,7 @@ public partial class Hitbox : Area2D
         if (body is Hurtbox hurtBox)
         {
             hurtBox.TakeDamage(Damage);
+            if(status != Hurtbox.Statuses.None) hurtBox.SetStatus(status, statusDuration);
         }
         EmitSignal(SignalName.EntityEntered);
     }
--- scripts/DamageHpSystem/Hurtbox.cs
+++ scripts/DamageHpSystem/Hurtbox.cs
@@ -4,10 +4,47 @@ using System;
 public partial class Hurtbox : Area2D
 {
     [Signal]
+    public delegate void EntityBurnEventHandler(double duration);
+    [Signal]
+    public delegate void EntityStunedEventHandler(double duration);
+    [Signal]
+    public delegate void EntityFreezedEventHandler(double duration);
+    [Signal]
+    public delegate void EntitySlowdownEventHandler(double duration);
+    [Signal]
     public delegate void EntityDeadEventHandler();
 
     [Export] private int maxHp;
     private int hp;
+
+    public enum Statuses
+    {
+        None,
+        Burn,
+        Stun,
+        Freeze,
+        Slowdown,
+
+    }
+
+    public void SetStatus(Statuses status, double duration)
+    {
+        switch (status)
+        {
+            case Statuses.Burn :
+                EmitSignal(SignalName.EntityBurn, duration);
+                break;
+            case Statuses.Stun :
+                EmitSignal(SignalName.EntityStuned, duration);
+                break;
+            case Statuses.Freeze :
+                EmitSignal(SignalName.EntityFreezed, duration);
+                break;
+            case Statuses.Slowdown :
+                EmitSignal(SignalName.EntitySlowdown, duration);
+                break;
+        }
+    }
     public void TakeDamage(int damage)
     {
         hp -= damage;
