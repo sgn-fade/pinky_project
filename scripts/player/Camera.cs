3a9853b582326fc476a58350aa153efe7618fe6e
fade <dimabreusgo@gmail.com>
2024-07-02 12:43:14 +0300

    camera refactoring

--- scripts/player/Camera.cs
+++ scripts/player/Camera.cs
@@ -5,26 +5,17 @@ namespace projectpinky.scripts.player;
 
 public partial class Camera : CharacterBody2D
 {
-    private Node2D focusedObject;
     private PlayerData player;
-    private Camera2D camera;
+    [Export] private Camera2D camera;
+
     public override void _Ready()
     {
         player = Global.Player;
-        camera = GetNode<Camera2D>("cam");
     }
 
     public override void _Process(double delta)
     {
-        if (focusedObject != null)
-        {
-            Velocity = (player.GetPosition() + (focusedObject.GlobalPosition - player.GetPosition()) / 4 -
-                        GlobalPosition) * 4;
-        }
-        else
-        {
-            Velocity = (player.GetPosition() - GlobalPosition) * 4;
-        }
+        Velocity = (player.GetPosition() - GlobalPosition) * 4;
         MoveAndSlide();
         UpdateCamera();
     }
@@ -35,24 +26,10 @@ public partial class Camera : CharacterBody2D
         {
             camera.Zoom = new Vector2(camera.Zoom.X - 0.05f, camera.Zoom.Y - 0.05f);
         }
+
         if (Input.IsActionJustReleased("wheel_down"))
         {
             camera.Zoom = new Vector2(camera.Zoom.X + 0.05f, camera.Zoom.Y + 0.05f);
-        }
-    }
-
-    public void SetView(Node2D obj)
-    {
-        if (focusedObject != null)
-        {
-            focusedObject.Call("set_focused", false);
-            focusedObject = null;
-            return;
-        }
-        if (obj != null)
-        {
-            obj.Call("set_focused", true);
-            focusedObject = obj;
         }
     }
 }
